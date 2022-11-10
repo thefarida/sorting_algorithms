438fd7eb30a940c20df71218c7f2b7e3e65d5482
thefarida <adamuibrahimfarida@gmail.com>
2022-11-10 13:38:36 +0000

    Insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,13 +1,13 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list
- * of integers in ascending order using
- * the Insertion sort algorithm.
- * @list: The doubly linked list.
- * 
- * Return: Nothing.
-*/
+  * insertion_sort_list - Sorts a doubly linked list
+  * of integers in ascending order using
+  * the Insertion sort algorithm.
+  * @list: The doubly linked list.
+  * 
+  * Return: Nothing.
+  */
 void insertion_sort_list(listint_t **list)
 {
     bool flag = false;
